a80c87f92a59f696259e1717a23f96e13e524e7b
Toon Rombaut <toon.rombaut@hotmail.com>
2021-12-16 22:16:48 +0100

    added water effect

--- src/assets/shader/fragment.glsl
+++ src/assets/shader/fragment.glsl
@@ -1,11 +1,21 @@
 uniform float time;
 uniform float progress;
-uniform sampler2D texture1;
+uniform sampler2D uTexture;
+uniform sampler2D uDisplacement;
 uniform vec4 resolution;
 varying vec2 vUv;
 varying vec3 vPosition;
 float PI = 3.141592653589793238;
 void main()	{
-	// vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);
-	gl_FragColor = vec4(vUv,0.0,1.);
+	//  vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);
+	 vec4 displacement = texture2D(uDisplacement,vUv);
+	 float theta = displacement.r * 2. * PI;
+
+	 vec2 dir = vec2(sin(theta),cos(theta));
+
+	 vec2 uv = vUv + dir*displacement.r*0.1;
+
+	 vec4 color = texture2D(uTexture,uv);
+
+	gl_FragColor = color;
 }
